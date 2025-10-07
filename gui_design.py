a4ed7dd1e6e7110a774240f3a6cac2ddebce2d74
Mohamed Khaled <139351096+Mohamed1khaled2@users.noreply.github.com>
2025-10-07 10:28:43 +0300

    The last updated exe

--- gui_design.py
+++ gui_design.py
@@ -15,25 +15,25 @@ class App(customtkinter.CTk):
 
         self.geometry("600x500")
         self.grid_columnconfigure((0, 1), weight=1)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
         self.title_label = customtkinter.CTkLabel(
             self, text="Convert TXT → CSV", font=("Arial", 28, "bold")
         )
         self.title_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-        self.button = customtkinter.CTkButton(
-            self, text="Select Text File", command=self.open_file
-        )
-        self.button.grid(row=0, column=0, padx=20, pady=20,
-                         sticky="ew", columnspan=2)
-
         self.name_file_label = customtkinter.CTkLabel(
             self, text="NameFile", font=("arial", 32)
         )
         self.name_file_label.grid(row=1, column=0, columnspan=2)
 
-        self.label_ready = customtkinter.CTkButton(
+        self.select_file_btn = customtkinter.CTkButton(
+            self, text="Select Text File", command=self.open_file
+        )
+        self.select_file_btn.grid(row=2, column=0, padx=20, pady=20,
+                                  columnspan=2)
+
+        self.convert_btn = customtkinter.CTkButton(
             self, text="Convert", font=("arial", 32), command=self.convert_fun
         )
 
@@ -45,16 +45,19 @@ class App(customtkinter.CTk):
         if self.name_file.find(".txt") != -1:
             self.name_file_label.configure(
                 text=self.name_file, text_color="black")
-            self.label_ready.grid(
-                row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2
+            self.convert_btn.grid(
+                row=3, column=0, padx=20, pady=20, sticky="ew", columnspan=2
             )
-
+            self.convert_btn.configure(fg_color="green")
         else:
             self.name_file_label.configure(
                 text="Your Choose Wrong File,\n please check extension of file is txt",
                 text_color="red",
                 font=("arial", 24),
             )
+            self.convert_btn.grid_forget()
+
+        self.select_file_btn.configure(text='Re-select File')
 
     def convert_fun(self):
 
